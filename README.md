8939df946951e504f62c9328b0e043ad4c5bc866
Pedro Sena <sena.pedro@gmail.com>
2014-02-10 15:04:32 -0300

    README

---
+++ README.md
@@ -0,0 +1,33 @@
+# db_wrapper - Wrap your DB calls in a transparent way 
+
+## What is db_wrapper ? ##
+
+db_wrapper is a Ruby gem that allows the creation of listeners to every database call you make in a transparent way.
+
+## How does it work ? ##
+
+It creates a super lightweight TCP proxy that will redirect every database call to the proxied database (in a **non blocking way**) to another process that will call the listeners you registered. Everything happens in a different process so the listeners won't impact the database query performance.
+
+The fact that it works like a proxy allows you to create listeners without changing your application code and without needing to worry about performance.
+
+## Which databases does it support ? ##
+
+* Mysql
+* PostgreSQL (future)
+* MongoDB (future)
+
+The protocol implementation is simple so you can easily extend it by yourself to support a
+different database (and send me the pull request if you want)
+
+## Next steps ##
+* PostgreSQL protocol
+* MongoDB protocol
+* Examples and documentation
+* Server listeners - Listeners that would get data sent from the server to the client, also in a non blocking way
+
+## License and copyright ##
+
+db_wrapper is copyrighted free software made available under the terms
+of either the GPL or Ruby's License.
+
+Copyright: (C) 2014 by Pedro Sena. All Rights Reserved.
--- lib/db_wrapper/database_proxy.rb
+++ lib/db_wrapper/database_proxy.rb
@@ -1,7 +1,7 @@
 module DBWrapper
   class DatabaseProxy
 
-    attr_reader :host, :port, :database_host, :database_port
+    attr_reader :host, :port, :database_host, :database_port, :listener_server_socket
     attr_accessor :protocol
 
     def initialize(host, port, database_host, database_port)
@@ -19,13 +19,13 @@ module DBWrapper
     def start!
       raise 'No protocol was given' if self.protocol.nil?
       listener_server_port = create_listener_server.addr[1]
-      listener_server_socket = TCPSocket.new(self.host, listener_server_port)
+      @listener_server_socket = TCPSocket.new(self.host, listener_server_port)
       database_proxy = self
       Proxy.start(host: @host, port: @port) do |conn|
         conn.server :database, host: database_proxy.database_host, port: database_proxy.database_port, relay_server: true
 
         conn.on_data do |data|
-          listener_server_socket.write_nonblock(data)
+          database_proxy.listener_server_socket.write_nonblock(data)
           data
         end
 
@@ -35,18 +35,16 @@ module DBWrapper
       end
     end
 
-    def stop!
-      Proxy.stop      
-    end
-
     def create_listener_server
       listener_server = TCPServer.new(host, 0) #Creates on first free port it can find
       client_listeners_controller = ListenersController.new @client_listeners
       fork do
         Socket.accept_loop(listener_server) do |connection|
-          while data = connection.readpartial(self.protocol.max_packet_size) do
-            client_listeners_controller.call_listeners(self.protocol, data)
-          end
+          begin
+            while data = connection.readpartial(self.protocol.max_packet_size) do
+              client_listeners_controller.call_listeners(self.protocol, data)
+            end
+          rescue Interrupt; end  #Thrown when you send a termination signal
         end          
       end
       listener_server      
